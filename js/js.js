9fe8c39d63d35358edd944ac97a0ab410384f4c9
Lenadiz74 <Lenadiz74@gmail.com>
2021-07-31 12:37:21 +0500

    JS

---
+++ js/js.js
@@ -0,0 +1,17 @@
+window.addEventListener('resize', changePosition);
+setTimeout(changePosition, 1);
+
+function changePosition(){
+    let width = window.innerWidth
+    if (width <= 2560) {
+        if (width > 1440) {
+            document.getElementById('header_positioned').setAttribute('style', 'left:' + (((width) / 2) - 586) + 'px');
+        } else if (width > 768) {
+            document.getElementById('header_positioned').setAttribute('style', 'left:' + 9 + '%');
+        } else {
+            document.getElementById('header_positioned').setAttribute('style', 'left:' + 'unset');
+        }
+    }else {
+        document.getElementById('header_positioned').setAttribute('style', 'left:' + 694 + 'px');
+    }
+}
